73eccc9a7d869c46eb12ba214ab93a4057744e4d
Jozef Nagy <jozefnagyjr@gmail.com>
2017-06-23 16:33:45 -0400

    part failure event

--- Initialize/makeEvents.py
+++ Initialize/makeEvents.py
@@ -66,5 +66,21 @@ response = requests.post(url, data=data_json, headers=headers)
 print(response)
 print(response.json())
 
+#Part failure
+EventData = {"datastoreId":1735,
+           "object_type":"event_failure",
+             "PID":162,
+             "VIN":"1734SD"}
+data_json = json.dumps(EventData)
+url="https://api.tierion.com/v1/records"
+headers = {'Content-type': 'application/json',
+           "X-Username":"jozefnagyjr@gmail.com",
+           "X-Api-Key":"ZquJZ80IrE/gSq+1V79SImwIyvMtYHY4Z+MzNNuou8o="}
+response = requests.post(url, data=data_json, headers=headers)
+print(response)
+print(response.json())
+
+
+
 
 
---
+++ jobBot.py
@@ -0,0 +1,5 @@
+#Jozef Nagy
+#6/23/17
+#Purpos: Search the blockchain for part failure events and take action.
+#The action inclues ordering the part and scheduling service.
+
