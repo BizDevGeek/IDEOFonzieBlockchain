f94300f5e5b26656bac44a793398074dfaa8f613
Jozef Nagy <jozefnagyjr@gmail.com>
2017-06-23 16:47:40 -0400

    gets list of all data objects for job search

--- jobBot.py
+++ jobBot.py
@@ -3,3 +3,17 @@
 #Purpos: Search the blockchain for part failure events and take action.
 #The action inclues ordering the part and scheduling service.
 
+import requests
+import json
+
+url_base="https://api.tierion.com/v1/records?datastoreId=1735"
+url = url_base
+headers = {'Content-type': 'application/json',
+           "X-Username":"jozefnagyjr@gmail.com",
+           "X-Api-Key":"ZquJZ80IrE/gSq+1V79SImwIyvMtYHY4Z+MzNNuou8o="}
+
+
+response = requests.get(url, headers=headers)
+print(response)
+print(response.json())
+
