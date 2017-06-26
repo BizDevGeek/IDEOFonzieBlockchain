fbad8a47e2d2623781f81ad74b72c86f8e8ae6ac
Jozef Nagy <jozefnagyjr@gmail.com>
2017-06-26 14:42:05 -0400

    cleanup

--- jobBot.py
+++ jobBot.py
@@ -14,9 +14,7 @@ def getObject(oid):
            "X-Username":"jozefnagyjr@gmail.com",
            "X-Api-Key":"ZquJZ80IrE/gSq+1V79SImwIyvMtYHY4Z+MzNNuou8o="}
     response = requests.get(url, headers=headers)
-    #print(url)
     #print(response)
-    #print(response.json())
     return response.json()
 
 
@@ -32,20 +30,15 @@ headers = {'Content-type': 'application/json',
 
 
 response = requests.get(url, headers=headers)
-#print(response)
-#print(response.json())
-
 raw_data = response.json()
 
 #filter all the returned objects to find:
 
-#loop through all json objects
+#loop through all json objects to find the group of data
 for key in raw_data:
     value = raw_data[key]
     #print("The key and value are ({}) = ({})".format(key, value))
     if key == "records":
-        #print("Found records:")
-        #print("The key and value are ({}) = ({})".format(key, value))
         events=value
         break
 pass
@@ -53,33 +46,14 @@ pass
 #Lookup each ID in the blockchain
 for x in events:
     #print(x)
-    #print(json.dumps(x))
-    #value = events[x]
-    #print("The key and value are ({}) = ({})".format(x, value))
-    #for y in x:
-        #print(y)
-    #z = json.loads(x)
-    #print(x["id"])
     eid = x["id"]
     json_obj = getObject(eid)
-    print(json_obj)
-
-    
+    #print(json_obj)
+    #Now check the "object_type" to see if it's an event to bid on.
+    for y in json_obj:
+        #json_y = json.loads(y)
+        #print(json_y["data"])
+        print(y)
         
-##    url_base="https://api.tierion.com/v1/records/"
-##    objectID=
-##    url = url_base + objectID
-##    headers = {'Content-type': 'application/json',
-##           "X-Username":"jozefnagyjr@gmail.com",
-##           "X-Api-Key":"ZquJZ80IrE/gSq+1V79SImwIyvMtYHY4Z+MzNNuou8o="}
-##    response = requests.get(url, headers=headers)
-
-
-#print(raw_data)
-#for key in raw_data:
-#    value = raw_data[key]
-#    print("The key and value are ({}) = ({})".format(key, value))
-#pass
-
-
+        
 
