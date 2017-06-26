6673dbe625633f92ad2fde2cd131c0f00d71c905
Jozef Nagy <jozefnagyjr@gmail.com>
2017-06-26 10:54:30 -0400

    converts data type

--- partsReader.py
+++ partsReader.py
@@ -8,24 +8,33 @@ import serial
 import json
 
 def swapPart(rfid):
-    print(rfid)
+    rfid = rfid  #[2:13] parsing 
+    print("Part swap detected: " + str(rfid))
+    writeRecord(rfid) 
     
 
 def writeRecord(dataPayload):
-    setattr(dataPayload, "datastoreId", 1735)
-    
-    headers = {'Content-type': 'application/json',
-           "X-Username":"jozefnagyjr@gmail.com",
-           "X-Api-Key":"ZquJZ80IrE/gSq+1V79SImwIyvMtYHY4Z+MzNNuou8o="}
+    #response = requests.post(baseUrl, data=json.dumps(dataPayload), headers=headers)
+    #print(response)
+    #print(response.json())
 
-    response = requests.post(baseUrl, data=json.dumps(dataPayload), headers=headers)
+    url = "https://ideo-autonet-node.run.aws-usw02-pr.ice.predix.io/api/vehicle/equipment/change"
+    
+    headers = {
+    'content-type': "application/json",
+    'x-api-key': "SsjA0MdYOGag8xSmLomllZ0wk2zp2s1GrXrBxhWuwt",
+    'cache-control': "no-cache"
+    }
+
+    response = requests.request("POST", url, data=json.dumps(dataPayload), headers=headers)
     print(response)
-    print(response.json())
+    print(response.text)
+
 
 baseUrl = "https://api.tierion.com/v1/records"
 with serial.Serial('\\.\COM3', 9600) as ser:
     while True:
         line = ser.readline()
-        swapPart(line.strip())
+        swapPart(line.strip().decode("utf-8") ) 
         #print(line.strip())
         
